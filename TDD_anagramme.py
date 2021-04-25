24cc8b782e528584a33495aa11a414cc8dc7d12e
AzzouzMezlini <azzouz.mezlini@gmail.com>
2021-04-25 11:49:18 +0200

    starting third test : make it pass

--- TDD_anagramme.py
+++ TDD_anagramme.py
@@ -5,8 +5,13 @@ class TestAreAnagrams(unittest.TestCase):
     def test_areanagrams(self):
         self.assertEqual(are_anagrams('', ''), False)
 
+    def test_areanagrams(self):
+        self.assertEqual(are_anagrams('a', 'a'), True)
+
 
 def are_anagrams(word_a: str, word_b: str):
+    if word_a == word_b:
+        return True
     return False
 
 
