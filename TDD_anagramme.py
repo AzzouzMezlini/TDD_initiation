8da8b555b79105a780cb8a60286b872a54f77814
AzzouzMezlini <azzouz.mezlini@gmail.com>
2021-04-25 12:07:20 +0200

    starting forth test : make it failed

--- TDD_anagramme.py
+++ TDD_anagramme.py
@@ -8,6 +8,12 @@ class TestAreAnagrams(unittest.TestCase):
     def test_areanagrams_a_a(self):
         self.assertEqual(are_anagrams('a', 'a'), True)
 
+    def test_areanagrams_vide_a(self):
+        self.assertEqual(are_anagrams('', 'a'), False)
+
+    def test_areanagrams_ba_ab(self):
+        self.assertEqual(are_anagrams('ba', 'ab'), True)
+
 
 def are_anagrams(word_a: str, word_b: str):
     if word_a == word_b and len(word_a) > 0:
