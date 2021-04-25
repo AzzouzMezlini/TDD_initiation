e5582fb0909990a1269a6b55ba007869f0e6e850
AzzouzMezlini <azzouz.mezlini@gmail.com>
2021-04-25 11:37:38 +0200

    starting first test : make it failed

--- TDD_anagramme.py
+++ TDD_anagramme.py
@@ -2,8 +2,13 @@ import unittest
 
 
 class TestAreAnagrams(unittest.TestCase):
-    pass
+    def test_areanagrams(self):
+        self.assertEqual(are_anagrams(), False)
 
 
 def are_anagrams():
     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
